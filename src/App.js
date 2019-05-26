68e9099dd4bc4a1d3fc832bd190cc78dd65b5483
Annakihab <Annaki.abdelbasset@gmail.com>
2019-05-26 21:12:11 +0000

    Updating files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Launcher from './components/launcher';
+import { persist, store } from './store';
+import AppRoutes from './router';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <PersistGate loading={ <Launcher/> } persistor={ persist }>
+          <AppRoutes/>
+        </PersistGate>
+      </Provider>
+    );
+  }
 }
 
 export default App;
+
+
